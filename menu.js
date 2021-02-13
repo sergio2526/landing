c6a8579d0bc414da5dd2a8d955d970bc0b47e4d1
sergio2526 <sergiorubiano25@hotmail.com>
2021-02-13 16:35:31 -0500

    primera version de mi landing

--- menu.js
+++ menu.js
@@ -1,10 +1,11 @@
-<html>
-<scrip>
-<df-messenger
+document.write(
+
+	<df-messenger
 		intent = "WELCOME"
 		chat-title = "sergiorubiano-cv-agent"
 		agent-id = "9b1bd1e4-ff2b-4cbe-a313-31f34eeab1ed"
 		language-code = "en"
 	></df-messenger>
-</scrip>
-</html>
+
+
+);
