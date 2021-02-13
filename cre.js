e22742a0c46e89ae9edeba34e3e5471c64c2ec7c
sergio2526 <sergiorubiano25@hotmail.com>
2021-02-13 16:24:53 -0500

    primera version de mi landing

--- cre.js
+++ cre.js
@@ -1,9 +1,10 @@
 <html>
+<scrip>
 <df-messenger
 		intent = "WELCOME"
 		chat-title = "sergiorubiano-cv-agent"
 		agent-id = "9b1bd1e4-ff2b-4cbe-a313-31f34eeab1ed"
 		language-code = "en"
 	></df-messenger>
-    
+</scrip>
 </html>
